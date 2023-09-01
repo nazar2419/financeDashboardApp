724a34fd8421d0ae6022593a3df1815b49a715a3
Nazar <nazarx095@gmail.com>
2023-09-02 00:36:56 +0300

    dashboard layout

---
+++ client/src/scenes/dashboard/Row1.tsx
@@ -0,0 +1,14 @@
+import DashboardBox from '@/components/DashboardBox'
+
+
+function Row1() {
+  return (
+    <>
+    <DashboardBox  gridArea="a"></DashboardBox>
+    <DashboardBox  gridArea="b"></DashboardBox>
+    <DashboardBox  gridArea="c"></DashboardBox>
+    </>
+  )
+}
+
+export default Row1
---
+++ client/src/scenes/dashboard/Row2.tsx
@@ -0,0 +1,13 @@
+import DashboardBox from '@/components/DashboardBox'
+
+function Row2() {
+  return (
+    <>
+      <DashboardBox  gridArea="d"></DashboardBox>
+      <DashboardBox  gridArea="e"></DashboardBox>
+      <DashboardBox  gridArea="f"></DashboardBox>
+    </>
+  )
+}
+
+export default Row2
