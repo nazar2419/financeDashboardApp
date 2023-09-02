a2d5dbc775f2cac5705a3bd7c78929647ee22def
Nazar <nazarx095@gmail.com>
2023-09-02 16:40:13 +0300

    redux toolkit setup and configuration

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,9 @@
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery } from '@/state/api';
 
 
 function Row1() {
+  const { data } = useGetKpisQuery();
   return (
     <>
     <DashboardBox  gridArea="a"></DashboardBox>
